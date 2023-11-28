f8b22f19ee0853251a946b68d222ec7801ea39a6
Ace <ace.sie.cwo@gmail.com>
2023-11-28 15:44:27 +0800

    [feat] 新增 Gradle 專案配置

---
+++ src/main/java/com/hao/Gradle_JWT_Init/Gradle_JWT_InitApplication.java
@@ -0,0 +1,13 @@
+package com.hao.Gradle_JWT_Init;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gradle_JWT_InitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gradle_JWT_InitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hao/Gradle_JWT_Init/GradleInitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hao.Gradle_JWT_Init;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleInitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
